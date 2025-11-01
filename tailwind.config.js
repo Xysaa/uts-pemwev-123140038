b83290d4c65f42c848b1e6b73cd2845c46db1f3a
Stevanus Cahya Anggara <stevanuschyaanggara@gmail.com>
2025-11-01 17:17:58 +0700

    refactor: clean up CSS and improve Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,21 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
-  theme: { extend: {} },
-  plugins: [],
+module.exports = {
+  darkMode: 'class',
+
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], 
+        serif: ["Playfair Display", "serif"], 
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/line-clamp'),
+  ],
 }
